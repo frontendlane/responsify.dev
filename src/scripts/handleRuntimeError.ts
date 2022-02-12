ead57d47ae3fcc22e7206ed375ac71fe8d41d262
frontendlane <43588352+frontendlane@users.noreply.github.com>
2022-02-12 05:51:00 +0100

    fix copy, spacing, hover

--- src/scripts/handleRuntimeError.ts
+++ src/scripts/handleRuntimeError.ts
@@ -1,7 +1,15 @@
 import { setContent, createElement } from "./domInteraction";
 
+const loadRuntimeErrorStyles = () => {
+    const link = createElement('link', { rel: 'stylesheet', href: './public/pages/index/index.runtime-error.css' })
+    // @ts-ignore
+    window.document.head.append(link);
+};
+
 export const handleRuntimeError = (error: Error) => {
+    loadRuntimeErrorStyles();
     window.document.getElementById('js-error')!.removeAttribute('hidden');
+    // TODO: put back disabled attributes
     const errorMessage = createElement('code', { class: 'inline-code' }, error.message) as Element;
     setContent(window.document.getElementById('js-error-intro'), ['JavaScript failed to execute with the following error message: ', errorMessage]);
 };
