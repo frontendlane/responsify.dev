ad36d792731169abefded583f8a91ddb6e2a073d
frontendlane <petar@frontendlane.com>
2023-11-05 14:52:45 +0100

    delete leftover files from merging two repositories

--- shell-scripts/scripts/scripts-cleanup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-# find and delete all 'scripts' folders from ./public/pages
-find ./public/pages -type d -name 'scripts' -exec rm -rf '{}' +
--- shell-scripts/styles/styles-cleanup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-# find and delete all .css files from ./public/pages
-find ./public/pages -type f -name '*.css' -delete
