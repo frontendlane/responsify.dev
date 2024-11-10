88b22b3b147b0bf3094025404b4d6640220a5e46
frontendlane <petar@frontendlane.com>
2024-11-10 20:53:39 +0100

    updated copy for browsersupport

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -16,11 +16,11 @@ export const Intro: FC = () => {
 				At my past job at a digital agency I transformed Photoshop, Sketch, and InVision designs into functioning
 				websites. But the designs gave no instructions on how to handle the in-between resolutions.{' '}
 				<span aria-hidden="true">☹️</span> So, to minimize the abrupt layout jumps I employed a technique I call
-				&quot;responsify&quot;, which uses CSS <Code>calc()</Code> function to <em>fluidly</em> change{' '}
-				<Code>width</Code>, <Code>margin</Code>, <Code>left</Code>, etc. at breakpoints.
+				responsify, which uses CSS <Code>calc()</Code> function to <em>fluidly</em> change <Code>width</Code>,{' '}
+				<Code>margin</Code>, <Code>left</Code>, etc. at breakpoints.
 			</p>
 			<p className="vertical-spacing">
-				Many told me they didn&apos;t understand what <em>exactly</em> it does until they saw it in action.{' '}
+				Many told me they didn&apos;t understand what responsify is/does until they saw it in action.{' '}
 				<span aria-hidden="true">💡</span> So, below&apos;s a screen recording of an example.
 			</p>
 
