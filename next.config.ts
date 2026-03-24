f7a35896ec74607179a0b888419ec94a834126e0
frontendlane <43588352+frontendlane@users.noreply.github.com>
2026-03-24 23:24:10 +0100

    prep for cloudflare deployment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
 	reactStrictMode: true,
-	output: 'standalone',
+	output: 'export',
 	reactCompiler: false, // compiler disabled until https://github.com/react-hook-form/react-hook-form/issues/12298 is resolved
 }
 
